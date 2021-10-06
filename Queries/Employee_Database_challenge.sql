00bafcff165db9e43f7fbf9c30b797861f5db916
ckeistler <88443672+ckeistler@users.noreply.github.com>
2021-10-06 16:10:47 -0500

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -6,9 +6,9 @@ DISTINCT on (e.emp_no, ttls.title)
 			ttls.title,
 			ttls.from_date,
 			ttls.to_date
--- INTO retirement_titles
+INTO retirement_titles1
 FROM titles AS ttls
-INNER JOIN employees AS e
+INNER JOIN current_emp AS e
 ON (e.emp_no = ttls.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 -- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
@@ -21,7 +21,8 @@ SELECT DISTINCT ON (rts.emp_no)
 rts.emp_no,
 rts.first_name, 
 rts.last_name,
-rts.title
+rts.title,
+rts.to_date
 INTO unique_titles
 FROM retirement_titles AS rts
 ORDER BY rts.emp_no ASC, to_date DESC;
@@ -32,3 +33,23 @@ INTO retiring_titles
 FROM unique_titles as uts
 GROUP BY uts.title
 ORDER BY COUNT DESC;
+
+
+SELECT 
+DISTINCT on (e.emp_no)
+			e.first_name,
+			e.last_name,
+			e.birth_date,
+			dpte.from_date,
+			dpte.to_date,
+			ttls.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS dpte
+ON (e.emp_no = dpte.emp_no)
+INNER JOIN titles as ttls
+ON (e.emp_no = ttls.emp_no)
+WHERE (dpte.to_date = '9999-01-01')
+	AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+-- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+ORDER BY e.emp_no ASC;
