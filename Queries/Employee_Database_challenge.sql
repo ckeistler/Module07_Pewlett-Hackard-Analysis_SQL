0b26ffba99846eb36bc1890021af8cf43c90fbf7
ckeistler <88443672+ckeistler@users.noreply.github.com>
2021-12-16 16:13:44 -0600

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,39 +1,49 @@
-SELECT 
-DISTINCT on (e.emp_no, ttls.title)
-			e.emp_no,
+SELECT  	e.emp_no,
 			e.first_name,
 			e.last_name,
 			ttls.title,
 			ttls.from_date,
 			ttls.to_date
-INTO retirement_titles1
+--INTO retirement_titles
 FROM titles AS ttls
-INNER JOIN current_emp AS e
+INNER JOIN employees AS e
 ON (e.emp_no = ttls.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
--- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
---	 AND (de.to_date = '9999-01-01');
 ORDER BY e.emp_no ASC;
-
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rts.emp_no)
 rts.emp_no,
 rts.first_name, 
 rts.last_name,
-rts.title,
-rts.to_date
-INTO unique_titles
+rts.title
+--INTO unique_titles
+FROM retirement_titles AS rts
+WHERE (rts.to_date = '9999-01-01')
+ORDER BY rts.emp_no ASC, to_date DESC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rts.emp_no)
+rts.emp_no,
+rts.first_name, 
+rts.last_name,
+rts.title
+--INTO unique_titles1
 FROM retirement_titles AS rts
 ORDER BY rts.emp_no ASC, to_date DESC;
 
 
 SELECT COUNT(uts.title), uts.title
-INTO retiring_titles
+--INTO retiring_titles
 FROM unique_titles as uts
 GROUP BY uts.title
 ORDER BY COUNT DESC;
 
+SELECT COUNT(uts.title), uts.title
+--INTO retiring_titles1
+FROM unique_titles1 as uts
+GROUP BY uts.title
+ORDER BY COUNT DESC;
 
 SELECT 
 DISTINCT on (e.emp_no)
@@ -43,7 +53,7 @@ DISTINCT on (e.emp_no)
 			dpte.from_date,
 			dpte.to_date,
 			ttls.title
-INTO mentorship_eligibility
+--INTO mentorship_eligibility
 FROM employees AS e
 INNER JOIN dept_emp AS dpte
 ON (e.emp_no = dpte.emp_no)
